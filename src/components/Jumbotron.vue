b2751cf0fbffcd61dfb9c4a77bed7ece2cb0bb40
Tim Mac <ttiimmaacc@gmail.com>
2020-06-24 22:10:00 +1000

    nav update

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-    <b-jumbotron id="bg-img" class=" text-white text-left rgba-stylish-strong py-5 px-4" fluid>
 
-      <b-container class="stroke">
+    <b-jumbotron id="bg-img" class="text-white text-left rgba-stylish-strong py-5 px-4" fluid>
+
+      <b-container>
         <b-row class="mt-50 mb-5">
           <b-col cols="12" md="6">
-          <h1><img alt="Vue logo" src="~@/assets/logo-hero.png" width="400px"></h1>
-            <h4 class="">MODULAR PUMP TRACKS</h4>
-            <p  class="">Continuus specialise. Wedesign and build modular pump tracks for indoor and outdoor enviroments. Helping children of all ages to stay active while enjoying the ride. Designed and manufactured in Canada.</p>
+          <img class="img-fluid" alt="logo" src="~@/assets/logo-hero.png" max-width="400px">
+            <h5 class="">MODULAR PUMP TRACKS</h5>
+            <p  class="">Continuus specialise. We design and build modular pump tracks for indoor and outdoor enviroments. Helping children of all ages to stay active while enjoying the ride.</p>
+            <p>Designed and manufactured in Canada.</p>
             <b-button variant="primary" href="#">CONTACT US</b-button>
             <b-link href="#">LEARN MORE</b-link>
           </b-col>
@@ -20,17 +21,17 @@
                   <b-col md="6" sm="12" class="mt-6"><h6>1. forming an unbroken whle, without interruption</h6></b-col>
                 </b-row>
               </b-col>
-              <b-col cols="4" class="mt-7"><img alt="icon logo" src="~@/assets/Icon-logo.png" width="340px"></b-col>
+              <b-col cols="4" class="mt-7 img-container"><img alt="icon logo" src="~@/assets/Icon-logo.png" width="340px"></b-col>
             </b-row>
 
     </b-container>
     </b-jumbotron>
-  </div>
+
 </template>
 <style>
 
 #bg-img {
-  background-image: linear-gradient(204.41deg, rgba(0, 0, 0, 0.49) 56.43%, rgba(0, 0, 0, 0.71) 88.88%), url('~@/assets/ramp-stroke.png'), url('~@/assets/bg-hero.png');
+  background-image: linear-gradient(204.41deg, rgba(0, 0, 0, 0.49) 56.43%, rgba(0, 0, 0, 0.71) 88.88%), url('~@/assets/stroke-outline.png'), url('~@/assets/bg-hero.png');
   min-height: 110vh;
   background-size: cover;
 }
@@ -41,10 +42,14 @@
   margin-top:15%;
 }
 .jumbotron{
+  /* margin-top: -75px!important; */
   margin-bottom: 0px!important;
+  /* height: 110vh!important; */
 }
 .mt-7{
   margin-top:7rem;
 }
 .mt-6{margin-top:5rem;}
+
+/* .img-container{overflow: hidden;} */
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-  <b-navbar toggleable="lg" type="dark" variant="dark" sticky=true >
+
+  <b-navbar class="" toggleable="lg" type="dark" sticky=true>
     <!-- Brand img -->
   <b-navbar variant="faded" type="dark">
     <b-navbar-brand href="#">
@@ -15,9 +15,25 @@
       <b-navbar-nav class="ml-auto">
         <b-nav-item href="#">TRACK SYSTEMS</b-nav-item>
         <b-nav-item href="#">CONTACT</b-nav-item>
-        <b-nav-item href="#" disabled><img height="20px" alt="logo" src="~@/assets/instagram.png"></b-nav-item>
+        <b-nav-item href="#"><img height="20px" alt="logo" src="~@/assets/instagram.png" class="ig"></b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
-</div>
 </template>
+
+<style>
+.navbar-nav{
+  /* flex-direction: row!important; */
+  /* position: relative!important; */
+}
+.navbar-collapse{margin-bottom: 0px;}
+.navbar-nav .nav-link{
+  padding-right:0.5rem!important;
+  padding-left:0.5rem!important;
+
+}
+.ig{
+  margin-top: -2px!important;
+}
+
+</style>
